1da76be704a4b22cb9a8c6cf725bdf54ccf4e91a
Dexton Anderson <contact@dextonanderson.com>
2019-06-19 10:52:42 -0500

    fix favicon

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
     <link rel="stylesheet" type="text/css" href="/css/main.css">
     <link rel="stylesheet" type="text/css" href="/css/jquery-eu-cookie-law-popup.css" />
     <!-- Global site tag (gtag.js) - Google Analytics -->
