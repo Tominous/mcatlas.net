7b139366fd9f63d8a41b88606b9e002efc9ab479
Dexton Anderson <contact@dextonanderson.com>
2019-06-19 09:49:43 -0500

    add Google Analytics

--- includes/head.php
+++ includes/head.php
@@ -7,4 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" type="text/css" media="screen" href="/main.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142397828-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-142397828-1');
+    </script>
 </head>
