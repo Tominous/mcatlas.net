128054480117728a587d27fe39ec431214348920
Dexton Anderson <contact@dextonanderson.com>
2019-06-19 12:56:33 -0500

    move stats into php include

---
+++ includes/stats.php
@@ -0,0 +1,22 @@
+<table style="display:inline;align-content:center;">
+    <tr>
+        <td>1</td>
+        <td>1:500 scale map of the Earth!</td>
+    </tr>
+    <tr>
+        <td>23</td>
+        <td>towns</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>countries</td>
+    </tr>
+    <tr>
+        <td>23</td>
+        <td>current players</td>
+    </tr>
+    <tr>
+        <td>5345</td>
+        <td>unique players</td>
+    </tr>
+</table>
--- index.php
+++ index.php
@@ -28,28 +28,7 @@
 
         <div class="content-section top-border">
             <h1>WORLD STATS</h1>
-            <table style="display:inline;align-content:center;">
-                <tr>
-                    <td>1</td>
-                    <td>1:500 scale map of the Earth!</td>
-                </tr>
-                <tr>
-                    <td>23</td>
-                    <td>towns</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>countries</td>
-                </tr>
-                <tr>
-                    <td>23</td>
-                    <td>current players</td>
-                </tr>
-                <tr>
-                    <td>5345</td>
-                    <td>unique players</td>
-                </tr>
-            </table>
+            <?php include('includes/stats.php') ?>
         </div>
 
         <div class="content-section top-border">
