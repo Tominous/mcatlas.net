b3223c2b0c744c46025f5bf74736ffba95f694b2
Dexton Anderson <contact@dextonanderson.com>
2019-06-19 18:27:52 -0500

    fix legal spacing

--- terms-and-privacy.php
+++ terms-and-privacy.php
@@ -6,30 +6,32 @@
 <body>
     <?php include('includes/nav.php') ?>
 
-    <div class="content content-section legal">
-        <h1>Terms of Service</h1>
-        When using the Services offered by this Website (MCATLAS.NET), you agree to adhere to these following Terms of Service:
+    <div class="content">
+        <div class="content-section legal">
+            <h1>Terms of Service</h1>
+            When using the Services offered by this Website (MCATLAS.NET), you agree to adhere to these following Terms of Service:
 
-        <ol class="legal-list">
-            <li>You will not attempt to cause damage (including but not limited to physical, emotional, financial) to the Website or its users.</li>
-            <li>You will not use any copyrighted material from this Website in a commercial endevor.</li>
-            <li>You will not misuse the Website for any type of personal gain (including but not limited to financial).</li>
-        </ol>
+            <ol class="legal-list">
+                <li>You will not attempt to cause damage (including but not limited to physical, emotional, financial) to the Website or its users.</li>
+                <li>You will not use any copyrighted material from this Website in a commercial endevor.</li>
+                <li>You will not misuse the Website for any type of personal gain (including but not limited to financial).</li>
+            </ol>
 
-        <h1>Privacy Policy</h1>
+            <h1>Privacy Policy</h1>
 
-        When using the Services offered by this Website (MCATLAS.NET), some information may be collected and temporarily stored on our secure servers.
+            When using the Services offered by this Website (MCATLAS.NET), some information may be collected and temporarily stored on our secure servers.
 
-        <h2>Information collected:</h2>
-        <ul class="legal-list">
-            <li>Your IP address</li>
-            <li>Your time of visitation (HTTP request)</li>
-        </ul>
+            <h2>Information collected:</h2>
+            <ul class="legal-list">
+                <li>Your IP address</li>
+                <li>Your time of visitation (HTTP request)</li>
+            </ul>
 
-        <p>MCATLAS does not collect any personally identifiable information from persons under the age of 13.</p>
+            <p>MCATLAS does not collect any personally identifiable information from persons under the age of 13.</p>
 
-        <h2>Google Analytics</h2>
-        <p>This Website uses Google Analytics, a web analysis service from Google Inc. Google Analytics uses so-called "Cookies", text files which are stored on your computer and which make it possible to analyse your use of the Website. The information that the cookie generates about your use of this Website (including your IP-address) is transferred to and stored on a server operated by Google in the United States. Google will use this information to evaluate your use of this Website, to compile reports on web activities for the site host and to perform other services associated with website and internet use. In some cases, Google may also share this information with third parties, to the extent this is legally required or to the extent that third parties process this data on commission from Google. In no case will Google associate your IP-address with other Google data. You can prevent cookies from installing using the appropriate settings in your browser; we must warn you however that in this case you may not be able to use all of the functions on this Website in their full scope. By using this Website, you give consent for Google to use the data about you acquired in the aforementioned manner and for the aforementioned purpose.</p>
+            <h2>Google Analytics</h2>
+            <p>This Website uses Google Analytics, a web analysis service from Google Inc. Google Analytics uses so-called "Cookies", text files which are stored on your computer and which make it possible to analyse your use of the Website. The information that the cookie generates about your use of this Website (including your IP-address) is transferred to and stored on a server operated by Google in the United States. Google will use this information to evaluate your use of this Website, to compile reports on web activities for the site host and to perform other services associated with website and internet use. In some cases, Google may also share this information with third parties, to the extent this is legally required or to the extent that third parties process this data on commission from Google. In no case will Google associate your IP-address with other Google data. You can prevent cookies from installing using the appropriate settings in your browser; we must warn you however that in this case you may not be able to use all of the functions on this Website in their full scope. By using this Website, you give consent for Google to use the data about you acquired in the aforementioned manner and for the aforementioned purpose.</p>
+        </div>
     </div>
 
     <?php include('includes/footer.php') ?>
