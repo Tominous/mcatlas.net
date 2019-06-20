a7c26bfb82070bfe7ea59dd0a5711fc175cc31dc
drewdorris <contact@dorrisd.com>
2019-06-20 13:21:40 -0400

    1000 instead of 500

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             <hr>
             <h1>EXPLORE MCATLAS</h1>
             <p>Have you ever wanted to play in a Minecraft world that is just like the real world?</p>
-            <p>MCATLAS hosts a 1:500 scale replica of the real world (except it's flat)! You can create towns, form
+            <p>MCATLAS hosts a 1:1000 scale replica of the real world (except it's flat)! You can create towns, form
                 countries, go to war, and most importantly, have loads of fun with friends on this unique server
                 experience.</p>
             <p>You can view a real-time map of the world <a href="/map">here!</a></p>
