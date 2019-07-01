9b59372c5318d6aadbf4f02af623344940228e74
Dexton Anderson <contact@dextonanderson.com>
2019-07-01 17:33:59 -0500

    edit vote

--- vote.php
+++ vote.php
@@ -27,8 +27,8 @@
                         </a>
                     </li>
                     <li>
-                        <a class="nav-link" href="javascript:;" onclick="setURL(&quot;https://minecraft-server-list.com/server/442029/vote/&quot;, this);">
-                            Minecraft-Server-List.com
+                        <a class="nav-link" href="javascript:;" onclick="setURL(&quot;&quot;, this);">
+                            Coming Soon
                         </a>
                     </li>
                 </ul>
