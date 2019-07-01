f33b56f0b005477977cb0fff364f8832aeac2bcd
drewdorris <contact@dorrisd.com>
2019-07-01 19:30:04 -0400

    more rules'

--- rules.php
+++ rules.php
@@ -9,16 +9,30 @@
     <div class="content">
         <h1>MCATLAS Rules</h1>
         <div style="text-align: left;">
-            <h2>World Rules</h2>
+            <h2>Server Rules</h2>
 
             <ol id="rule-list">
-                <li class="rule-list-entry">Do not deface or destory the surface of the world.</li>
+				<li class="rule-list-entry">Do not use client-side cheats or hacks. You may use mods that do not provide a large advantage in gameplay (Optifine, minimaps, etc.)</li>
+                <li class="rule-list-entry">Do not exploit any server-side bugs. <a href="discord.gg/srSSSgJ">Report them!</a></li>
+                <li class="rule-list-entry">Do not attempt to lag or cause harm to the server.</li>
+				<li class="rule-list-entry">No inappropriate material (builds, chat, town names, etc). Includes swearing. Keep it PG.</li>
+				<li class="rule-list-entry">No harassing or bullying other players.</li>
+				<li class="rule-list-entry">No spamming or flooding chat.</li>
+                <li class="rule-list-entry">Do not advertise other servers or products.</li>
             </ol>
 
-            <h2>Community Rules</h2>
+			<h2>Town and World Rules</h2>
 
-            <ol id="rule-list">
-                <li class="rule-list-entry">Do not deface or destory the surface of the world.</li>
+			<ol id="rule-list">
+			    <li class="rule-list-entry">Do not significantly alter or destory the surface of the world. You can cut down a tree, but don't blow up a mountain.</li>
+				<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating towns, etc).</li>
+				<li class="rule-list-entry">Towns can be named anything, but we recommend naming them after real life places. If you go to Chicago, you expect to see Chicago, not HerobrineCity.</li>
+				<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, geographical feature, or city. You can add title such as "Empire" to the end of them.</li>
+				<li class="rule-list-entry">Towns and nation names shouldn't be so similar to another town or nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
+				<li class="rule-list-entry">Towns can be any distance away from a nation capital. The nation's name must be relevant to the nation's capital's location.</li>
+			    <li class="rule-list-entry">Griefing or destroying land around towns is not allowed.</li>
+			    <li class="rule-list-entry">If a town is abandoned with no claims to it, you may claim/loot/destroy it.</li>
+				<li class="rule-list-entry">Do not create a town next to another town just to block the other town's growth. If you're a nearby town, you are allowed to expand next to it.</li>
             </ol>
         </div>
     </div>
