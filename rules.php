d7df063a1217e8c84442296b2893f3302eccc092
Dexton Anderson <contact@dextonanderson.com>
2019-07-09 20:38:15 -0500

    fix rules

--- rules.php
+++ rules.php
@@ -1,48 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include('includes/head.php') ?>
+<head>
+	<title>MCATLAS - A worldly Minecraft server</title>
+	<meta name="description" content="Play Minecraft on a map of the Earth! MCATLAS hosts a 1:1000 scale map of the Earth in Minecraft!">
+	<meta name="keywords" content="minecraft,mc,earth,world,map,server,survival,towns,towny,mcatlas,atlasmc,eco,econ,ecnomomy,gold,dynmap,vote">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/css/jquery-eu-cookie-law-popup.css">
+
+	<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-142397828-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-142397828-1');
+	</script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script defer="" src="/js/main.js"></script>
+	<script async="" src="/js/jquery-eu-cookie-law-popup.js"></script>
+</head>
 
 <body>
-    <?php include('includes/nav.php') ?>
-
-    <div class="content">
-        <h1>MCATLAS Rules</h1>
-        <div style="text-align: left; z-index: 100; position: relative">
-            <h2>General Rules</h2>
-
-            <ol id="rule-list">
-				<li class="rule-list-entry">Do not use client-side cheats or hacks. You may use mods that do not provide a large advantage in gameplay (Optifine, minimaps, etc.)</li>
-                <li class="rule-list-entry">Do not exploit any server-side bugs. <a href="https://discord.gg/srSSSgJ">Report them!</a></li>
-                <li class="rule-list-entry">Do not attempt to lag or cause harm to the server.</li>
-				<li class="rule-list-entry">No inappropriate material (builds, chat, town names, etc). Includes swearing. Keep it PG.</li>
-				<li class="rule-list-entry">No harassing or bullying other players.</li>
-				<li class="rule-list-entry">No spamming or flooding chat.</li>
-                <li class="rule-list-entry">Do not advertise other servers or products.</li>
-            </ol>
-
-			<h2>Town and World Rules</h2>
-
-			<ol id="rule-list">
-			    <li class="rule-list-entry">Do not significantly alter or destroy the surface of the world. You can cut down a tree, but don't blow up a mountain.</li>
-				<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating towns, etc).</li>
-				<li class="rule-list-entry">Do not block waterways. Being able to travel by water around the map is important!</li>
-				<li class="rule-list-entry">Towns can be named anything, but we recommend naming them after real life places. If you go to Chicago, you expect to see Chicago, not HerobrineCity.</li>
-				<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, geographical feature, or city. You can add titles such as "Empire" to the end of them.</li>
-				<li class="rule-list-entry">Towns and nation names shouldn't be so similar to another town or nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
-				<li class="rule-list-entry">Towns can be any distance away from a nation capital. The nation's name must be relevant to the nation's capital's location.</li>
-			    <li class="rule-list-entry">Stealing is not allowed. Lock your chests to prevent this.</li>
-				<li class="rule-list-entry">Griefing or destroying things at the edge of a town's claim is not allowed.</li>
-			    <li class="rule-list-entry">If a town/build has no claims to it and is not near a claim, you may claim/loot/destroy it (but please don't make it ugly).</li>
-				<li class="rule-list-entry">Griefing canals, bridges, paths, and other transportation methods in unclaimed areas is not allowed.</li>
-				<li class="rule-list-entry">Do not create a town next to another town just to block the other town's growth. If you're a nearby town, you are allowed to expand next to it.</li>
-				<li class="rule-list-entry">Spawn killing or trapping a player is not allowed.</li>
-				<li class="rule-list-entry">Killing players is not allowed (unless PvP is enabled by the town). If you can't PvP, don't use lava/etc to kill someone.</li>
-            </ol>
-        </div>
-    </div>
-
-    <?php include('includes/footer.php') ?>
+	<div class="bg" style="transform: translate3d(-3.09712%, -3.23858%, 0px);"></div>
+	<div class="bg-trans"></div>
+	<nav class="navbar">
+		<a class="button" href="/">HOME</a> <a class="button gold-text" href="/vote">VOTE</a>
+		<a class="button" href="/map">MAP</a>
+		<a class="button" href="/rules">RULES</a>
+		<a class="button" href="https://mcatlas.fandom.com">WIKI</a>
+		<a class="button" href="/bans">BANS</a>
+		<a class="button" href="https://discordapp.com/invite/srSSSgJ">DISCORD</a>
+	</nav>
+	<div class="eupopup eupopup-bottom"></div>
+	<div class="content-minified">
+		<div class="content-section">
+			<h1>MCATLAS Rules</h1>
+			<div style="text-align: left; z-index: 100; position: relative">
+				<h3>General</h3>
+				<ol id="rule-list">
+					<li class="rule-list-entry">Do not use client-side cheats or hacks. You may use mods that do not provide a large advantage in gameplay (Optifine, minimaps, etc.)</li>
+					<li class="rule-list-entry">Do not exploit any server-side bugs. <a href="https://discord.gg/srSSSgJ">Report them!</a></li>
+					<li class="rule-list-entry">Do not attempt to lag or cause harm to the server.</li>
+					<li class="rule-list-entry">No inappropriate material (builds, chat, town names, etc). Includes swearing. Keep it PG.</li>
+					<li class="rule-list-entry">No harassing or bullying other players.</li>
+					<li class="rule-list-entry">No spamming or flooding chat.</li>
+					<li class="rule-list-entry">Do not advertise other servers or products.</li>
+				</ol>
+				<h3>Builds</h3>
+				<ol id="rule-list">
+					<li class="rule-list-entry">Do not significantly alter or destroy the surface of the world. You can cut down a tree, but don't blow up a mountain.</li>
+					<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating towns, etc).</li>
+					<li class="rule-list-entry">Do not create large islands (more than 1x1 chunk) in oceans. You are not allowed to create multiple islands in one area.</li>
+					<li class="rule-list-entry">Do not block waterways. Being able to travel by water around the map is important!</li>
+				</ol>
+				<h3>Griefing and Theft</h3>
+				<ol id="rule-list">
+					<li class="rule-list-entry">Griefing or destroying things at the edge of a town's claim is not allowed. Mining near claims is fine.</li>
+					<li class="rule-list-entry">If a town/build has no claims to it and is not near a claim, you may claim/loot/destroy it (but please don't make it ugly).</li>
+					<li class="rule-list-entry">Griefing canals, bridges, paths, and other transportation methods in unclaimed areas is not allowed.</li>
+					<li class="rule-list-entry">Stealing is not allowed. Lock your chests to prevent this.</li>
+				</ol>
+				<h3>Towny (Town and Nations)</h3>
+				<ol id="rule-list">
+					<li class="rule-list-entry">Towns can be named anything, but we recommend naming them after real life places. If you go to Chicago, you expect to see Chicago, not HerobrineCity.</li>
+					<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, geographical feature, or city. You can add titles such as "Empire" to the end of them.</li>
+					<li class="rule-list-entry">Towns and nation names shouldn't be so similar to another town or nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
+					<li class="rule-list-entry">Towns can be any distance away from a nation capital. The nation's name must be relevant to the nation's capital's location.</li>
+					<li class="rule-list-entry">Do not create a town next to another town just to block the other town's growth. If you're a nearby town, you are allowed to expand next to it.</li>
+				</ol>
+				<h3>PvP</h3>
+				<ol id="rule-list">
+					<li class="rule-list-entry">Spawn killing or trapping a player is not allowed.</li>
+					<li class="rule-list-entry">Killing players is not allowed (unless PvP is enabled by the town). If you can't PvP, don't use lava/etc to kill someone.</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+	<img id="antartica-bg" src="/img/antartica.png" alt="antartica">
+	<div class="footer">
+		<div id="copyright">
+			<img src="/img/ruinscraft-scaled.png" style="width:5rem;height:auto;padding:15px;" alt="ruinscraft-logo">
+			<p>
+				MCATLAS is a Minecraft server by <a href="https://ruinscraft.com">Ruinscraft</a>.
+				©
+				2019 Ruinscraft.
+			</p>
+			<p>Minecraft is a registered trademark of Microsoft Corporation.</p>
+		</div>
+		<div id="social">
+			<p>
+				<a href="/terms-and-privacy">Terms and Privacy</a><br>
+				<a href="/contact">Contact</a><br>
+				<a href="https://twitter.com/ruinscraft">Twitter</a><br>
+				<a href="https://facebook.com/RuinscraftMinecraftServer">Facebook</a><br>
+				<a href="https://github.com/Ruinscraft">GitHub</a><br>
+				<a href="https://www.youtube.com/user/royalkingkb">YouTube</a><br>
+			</p>
+		</div>
+	</div>
 </body>
 
 </html>
