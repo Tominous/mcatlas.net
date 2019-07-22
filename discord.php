58729c29c130137ae042f3d49db5c61ff750caf9
Dexton Anderson <contact@dextonanderson.com>
2019-07-22 08:16:44 -0500

    redirects

---
+++ discord.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=https://discord.gg/srSSSgJ">
+  </head>
+</html>
---
+++ wiki.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=https://github.com/Ruinscraft/mcatlas-wiki/wiki">
+  </head>
+</html>
