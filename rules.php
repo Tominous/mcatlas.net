6693a55690f725cc800bfc53e7ea5e4eadf21e9c
Dexton Anderson <contact@dextonanderson.com>
2019-07-25 08:52:21 -0500

    rules capitalization

--- rules.php
+++ rules.php
@@ -15,7 +15,7 @@
 					<li class="rule-list-entry">Do not use client-side cheats or hacks. You may use mods that do not provide a large advantage in gameplay (Optifine, minimaps, etc.)</li>
 					<li class="rule-list-entry">Do not exploit any server-side bugs. <a href="https://discord.gg/srSSSgJ">Report them!</a></li>
 					<li class="rule-list-entry">Do not attempt to lag or cause harm to the server.</li>
-					<li class="rule-list-entry">No inappropriate material (builds, chat, town names, etc). Includes swearing. Keep it PG.</li>
+					<li class="rule-list-entry">No inappropriate material (builds, chat, Town names, etc). Includes swearing. Keep it PG.</li>
 					<li class="rule-list-entry">No harassing or bullying other players.</li>
 					<li class="rule-list-entry">No spamming or flooding chat.</li>
 					<li class="rule-list-entry">Do not advertise other servers or products.</li>
@@ -23,7 +23,7 @@
 				<h3>Builds</h3>
 				<ol id="rule-list">
 					<li class="rule-list-entry">Do not significantly alter or destroy the surface of the world. You can cut down a tree, but don't blow up a mountain.</li>
-					<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating towns, underwater cities, etc).</li>
+					<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating Towns, underwater cities, etc).</li>
 					<li class="rule-list-entry">Do not create large islands (more than 1x1 chunk) in oceans. You are not allowed to create multiple islands in one area.</li>
 					<li class="rule-list-entry">Do not block waterways. You may build bridges; they must be at least 2 blocks above the surface of the water so boats may pass underneath.</li>
 					<li class="rule-list-entry">Do not build bridges more than 4 blocks wide across large oceans or bays. Typically, ice bridges with side walls or railroad bridges are a good idea.</li>
@@ -32,21 +32,21 @@
 				<ol id="rule-list">
 					<li class="rule-list-entry">Griefing is not allowed.</li>
 					<li class="rule-list-entry">Griefing of unclaimed railways, paths, portals, hubs, etc. is not allowed.</li>
-					<li class="rule-list-entry">If a build has no claims to it and is not near another claim, you may claim/loot/destroy it (but please don't make it ugly). If it looks like it's part of a nearby town, don't touch it!</li>
+					<li class="rule-list-entry">If a build has no claims to it and is not near another claim, you may claim/loot/destroy it (but please don't make it ugly). If it looks like it's part of a nearby Town, don't touch it!</li>
 					<li class="rule-list-entry">Theft of items is discouraged but allowed. Town or Nation bank theft is easily provable and will result in heavy punishment. Ensure you lock your chests to prevent item theft by placing a sign on them.</li>
 				</ol>
-				<h3>Towny (Town and Nations)</h3>
+				<h3>Towny (Towns and Nations)</h3>
 				<ol id="rule-list">
 					<li class="rule-list-entry">Towns can be named anything, but we recommend naming them after real life places. If you go to Chicago, you expect to see Chicago, not EpicBurgerTown.</li>
-					<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, geographical feature, or city. You can add titles such as "Empire" to the end of them.</li>
-					<li class="rule-list-entry">Towns and nation names shouldn't be so similar to another town or nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
-					<li class="rule-list-entry">Towns can be any distance away from a nation capital. The nation's name must be relevant to the nation's capital's location.</li>
-					<li class="rule-list-entry">Do not create a town or claim land close to another town just to block the other town's growth. You are allowed to expand near/next to it if given permission by the original town. Don't go out of your way to claim near their town!</li>
+					<li class="rule-list-entry">Nations must be named after a formerly or currently existing Nation, territory, geographical feature, or city. You can add titles such as "Empire" to the end of them.</li>
+					<li class="rule-list-entry">Towns and Nation names shouldn't be so similar to another Town or Nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
+					<li class="rule-list-entry">Towns can be any distance away from a Nation capital. The Nation's name must be relevant to the Nation's capital's location.</li>
+					<li class="rule-list-entry">Do not create a Town or claim land close to another Town just to block the other Town's growth. You are allowed to expand near/next to it if given permission by the original Town. Don't go out of your way to claim near their Town!</li>
 				</ol>
 				<h3>PvP</h3>
 				<ol id="rule-list">
-					<li class="rule-list-entry">Trapping a player (portal traps, etc.) or killing a player repeatedly at their spawnpoint is not allowed (even if the town has PvP enabled).</li>
-					<li class="rule-list-entry">Killing players is not allowed (unless PvP is enabled by the town). If you can't PvP, don't use other methods (lava, etc.) to kill someone.</li>
+					<li class="rule-list-entry">Trapping a player (portal traps, etc.) or killing a player repeatedly at their spawnpoint is not allowed (even if the Town has PvP enabled).</li>
+					<li class="rule-list-entry">Killing players is not allowed (unless PvP is enabled by the Town). If you can't PvP, don't use other methods (lava, etc.) to kill someone.</li>
 				</ol>
 			</div>
 		</div>
