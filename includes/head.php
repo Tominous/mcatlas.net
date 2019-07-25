8e0c0b3be9a9850619c373989b4c0cd5a9c50c76
Dexton Anderson <contact@dextonanderson.com>
2019-07-25 08:17:16 -0500

    fix

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <head>
-    <title>MCAtlas - A worldly Minecraft server</title>
+    <title>MCATLAS - A worldly Minecraft server</title>
     <meta name="description" content="Play Minecraft on a map of the Earth! MCATLAS hosts a 1:1000 scale map of the Earth in Minecraft!">
-    <meta name="keywords" content="minecraft,mc,earth,world,map,server,survival,towns,towny,mcatlas,eco,econ,ecnomomy,gold,dynmap,vote">
+    <meta name="keywords" content="minecraft,mc,earth,world,map,server,survival,towns,towny,mcatlas,atlasmc,eco,econ,ecnomomy,gold,dynmap,vote">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
