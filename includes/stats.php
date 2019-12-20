21e49bb610c2fdb804e01c975fe9d90181cb1863
Dexton Anderson <contact@dextonanderson.com>
2019-12-19 20:16:07 -0600

    add unique

--- includes/stats.php
+++ includes/stats.php
@@ -13,14 +13,17 @@ if ($mysql_conn->connect_error) {
 $mysqlq_town_count = "select count(name) from TOWNY_TOWNS;";
 $mysqlq_nation_count = "select count(name) from TOWNY_NATIONS;";
 $mysqlq_resident_count = "select count(name) from TOWNY_RESIDENTS;";
+$mysqlq_unique_count = "select count(playerid) from `lb-players`;";
 
 $rs_town_count = mysqli_query($mysql_conn, $mysqlq_town_count);
 $rs_nation_count = mysqli_query($mysql_conn, $mysqlq_nation_count);
 $rs_resident_count = mysqli_query($mysql_conn, $mysqlq_resident_count);
+$rs_unique_count = mysqli_query($mysql_conn, $mysqlq_unique_count);
 
 $town_count = $rs_town_count->fetch_row()[0];
 $nation_count = $rs_nation_count->fetch_row()[0];
 $resident_count = $rs_resident_count->fetch_row()[0];
+$unique_count = $rs_unique_count->fetch_row()[0];
 
 ?>
 
@@ -40,5 +43,9 @@ $resident_count = $rs_resident_count->fetch_row()[0];
     <tr>
         <td><?php echo $resident_count; ?></td>
         <td>active town members</td>
+    </tr>
+    <tr>
+        <td><?php echo $unique_count; ?></td>
+        <td>unique players have joined</td>
     </tr>
 </table>
